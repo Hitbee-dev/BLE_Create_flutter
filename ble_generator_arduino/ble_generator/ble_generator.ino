6232383c40145a337e5cbea55791df0722eeec98
Hitbee-dev <ckim.esw@gmail.com>
2021-08-05 20:08:09 +0900

    arduino code modify

--- ble_generator_arduino/ble_generator/ble_generator.ino
+++ ble_generator_arduino/ble_generator/ble_generator.ino
@@ -1,75 +1,102 @@
-#include <SoftwareSerial.h> 
-#include <string.h>
-#define TX 2
-#define RX 3
+#include <SoftwareSerial.h>
+SoftwareSerial HM10(2,3); // RX, TX
 
-//UUID ECBB5C5A458340228C89C4ADFC3071BE
-SoftwareSerial BTSerial(TX, RX);
-boolean checker = false;
-unsigned long time=0,pre_time=0,buf=0;
-byte bt_in;
-char bf;
-int T_count=0;
-byte data;
-char buff[200];
-unsigned char buff_idx = 0;
-unsigned char isParse = 0;
-char uuid= "ECBB5C5A458340228C89C4ADFC3071BE";
+// Check Sec
+#define SLEEP_TIME 1000 // (5, 10, 15) ms
+#define MAX_COUNT 1
+// Check MAC
+String CHECK_UUID = "39ED98FF2900441A802F9C398FC199D2";
+
+// Parse Buffer
+byte buff[255] = {0};
+byte buff_idx = 0;
+
+// counting
+int success = 0, fail = 0;
+int count = MAX_COUNT;
+bool check = false;
+bool start = false;
+bool sleepMode = false;
+bool wakeUp = true;
+unsigned long cur = 0;
+unsigned long pre = 0; //이전시간
+const long delayTime = 300; //잠들었다가 깨는 0.3초 대기시간
+String dummyName = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 
 void setup() {
-   Serial.begin(9600);
-   //pinMode(RX, INPUT);    //UART pin for Bluetooth
-   //pinMode(TX, OUTPUT);
-   BTSerial.begin(9600);
-   //BTSerial.println("AT+ROLE1");
-   //BTSerial.println("AT+IMME1");//IMME[] 모듈 동작 타입 설정
+  //기본 통신속도는 9600입니다.
+  Serial.begin(9600);
+  HM10.begin(9600);
+//  Serial.println("Arduino UNO Start!");
 }
 
-void loop(){
-  if (BTSerial.available()) {
-    Serial.write(BTSerial.read());
+void loop() {
+  //HM10.println(dummyName);
+  // Buffer check
+  if (!wakeUp) {
+    HM10.println(dummyName);
   }
-  if (Serial.available()) {
-    BTSerial.write(Serial.read());
+  if (HM10.available()) {
+    wakeUp = true;
+    byte data = HM10.read();
+    buff[buff_idx++] = data;
+    if (data == '\n') {
+      buff[buff_idx] = '\0';
+      parse_buffer();
+    }
+    Serial.write(data);
   }
- /*
-  if (BTSerial.available()) {
-    char c = BTSerial.read();
-    Serial.print(c);
-    /*buff[buff_idx++] = c;
-    
-    if (c == '\n') {
-      buff[buff_idx++] = '\0';
-      buffParse();
-    }*/
-  
-}
 
-
-void buffParse() {
-  char *ptr;
-  unsigned char idx;
-  ptr = strtok(buff, "+");
-  ptr += strlen(ptr) + 1;
-  if (!strncmp(ptr, "DISCE", 5)) {
-  }
-  else if (!strncmp(ptr, "DISC", 4)) {
-    ptr = strtok(ptr, ":");
-    while (ptr != NULL) {
-      if (strlen(ptr) == 32) {
-      // Serial.println(ptr);
-       int com = strcmp(ptr,uuid);
-       if(com == 0){ 
-          checker = true;
-          }
-        else{
-        
-          checker = false;
-           }
-      }
-      ptr = strtok(NULL, ":");
-      
+  if (!sleepMode && wakeUp) {
+    if (count > 0 && !start) {
+      count--;
+      delay(100);
+      HM10.println("AT+DISI?");
+      start = true;
+    } else if (count == 0 && !start) {
+      count = MAX_COUNT;
+      Serial.print("\nSUCCESS : ");
+      Serial.println(success);
+      Serial.print("Fail : ");
+      Serial.println(fail);
+      HM10.println("AT+SLEEP");
+      pre = millis();
+      sleepMode = true;
+    }
+    if (Serial.available()) {
+      byte data = Serial.read();
+      HM10.write(data);
     }
   }
+  else {
+    checkTime();
+  }
+}
+
+void parse_buffer(){
+  String line = buff;
+  //Serial.println();
+  if (line.substring(0, 8).equals("OK+DISC:")) {
+    String mac = line.substring(17, 49);
+    //Serial.println(mac);
+    if (mac.equals(CHECK_UUID)) {
+      check = true;
+    }
+  }
+  if (line.substring(0, 8).equals("OK+DISCE")) {
+    check ? success++ : fail++;
+    check = false;
+    start = false;
+  }
   buff_idx = 0;
+}
+
+void checkTime() {
+  cur = millis();
+  if(cur - pre >= delayTime) {
+    //Serial.println("SEND WAKEUP");
+    wakeUp = false;
+    count = MAX_COUNT;
+    sleepMode = false;
+  } 
 }
